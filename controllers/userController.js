34d8b8f49701e97dd9304f97fc26cdd123f57c89
K4R-R <karanr4ghu@gmail.com>
2024-05-21 17:25:47 +0530

    another silly mistake

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,7 @@ const register = async (req,res) => {
       const user = new User({
          name:req.body.name,
          email:req.body.email,
-         image:"UserImages/" + req.file.filename,
+         image:"userImages/" + req.file.filename,
          password: passwordHash
       });
 
